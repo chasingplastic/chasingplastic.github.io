c0f925c6c7f47d5e84d9dbfa60612ee4bee60429
Dwij <dwij.mehta@gmail.com>
2020-08-22 18:24:31 +0530

    serving static files

---
+++ precache-manifest.bfff949edd3f6aba44fb4bd604a2f404.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b65faf4854936b78a32e8f7c41da9d38",
+    "url": "/chasingplastic/chasingplastic.github.io/index.html"
+  },
+  {
+    "revision": "c42fb67d243773011ab3",
+    "url": "/chasingplastic/chasingplastic.github.io/static/css/2.6c327026.chunk.css"
+  },
+  {
+    "revision": "1dd74b6d462db9704722",
+    "url": "/chasingplastic/chasingplastic.github.io/static/css/main.391f93b2.chunk.css"
+  },
+  {
+    "revision": "c42fb67d243773011ab3",
+    "url": "/chasingplastic/chasingplastic.github.io/static/js/2.055e13e5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/chasingplastic/chasingplastic.github.io/static/js/2.055e13e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dd74b6d462db9704722",
+    "url": "/chasingplastic/chasingplastic.github.io/static/js/main.fbf6755f.chunk.js"
+  },
+  {
+    "revision": "9ba090c20c0d5668ac55",
+    "url": "/chasingplastic/chasingplastic.github.io/static/js/runtime-main.30c6d2c8.js"
+  },
+  {
+    "revision": "c936a3d6ea63e7303aca34c494a80626",
+    "url": "/chasingplastic/chasingplastic.github.io/static/media/Component 2 – 1.c936a3d6.png"
+  },
+  {
+    "revision": "ff0ca3f7d3135deeadbabb1fb087b6d8",
+    "url": "/chasingplastic/chasingplastic.github.io/static/media/Group-laughing.ff0ca3f7.jpg"
+  },
+  {
+    "revision": "a3eea3c362d8ae039b27d4dddd75a905",
+    "url": "/chasingplastic/chasingplastic.github.io/static/media/arnav.a3eea3c3.png"
+  },
+  {
+    "revision": "b9bd78f35e22cd14945d106e0f7b095c",
+    "url": "/chasingplastic/chasingplastic.github.io/static/media/contact.b9bd78f3.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/chasingplastic/chasingplastic.github.io/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/chasingplastic/chasingplastic.github.io/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/chasingplastic/chasingplastic.github.io/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/chasingplastic/chasingplastic.github.io/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/chasingplastic/chasingplastic.github.io/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "31cf96ce2a7f26e3c778b4e69ea7c3a9",
+    "url": "/chasingplastic/chasingplastic.github.io/static/media/insta logo.31cf96ce.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/chasingplastic/chasingplastic.github.io/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "dfa64485d1f0e6ab0df986d6b1d8c4ec",
+    "url": "/chasingplastic/chasingplastic.github.io/static/media/picking plastic.dfa64485.jpg"
+  },
+  {
+    "revision": "bab3acd22fdf62d08d1e389be1ea4ac4",
+    "url": "/chasingplastic/chasingplastic.github.io/static/media/sanjana.bab3acd2.png"
+  },
+  {
+    "revision": "8b585c88a21ce8e0afe79bcc59705923",
+    "url": "/chasingplastic/chasingplastic.github.io/static/media/speaker.8b585c88.jpg"
+  },
+  {
+    "revision": "2e03dd20ea902b146f60aec84ea506c7",
+    "url": "/chasingplastic/chasingplastic.github.io/static/media/spotify logo.2e03dd20.png"
+  },
+  {
+    "revision": "398c4f033fe1711057828d28da1c5bbb",
+    "url": "/chasingplastic/chasingplastic.github.io/static/media/trialman-1.398c4f03.jpg"
+  },
+  {
+    "revision": "35928daf03a3f78624007ee523f240e6",
+    "url": "/chasingplastic/chasingplastic.github.io/static/media/trialwoman-1.35928daf.jpeg"
+  }
+]);
