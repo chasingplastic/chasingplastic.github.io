6eb8ca4c5275ff25eb07464554d70e62daeb5414
Dwij <dwij.mehta@gmail.com>
2020-08-24 15:07:08 +0530

    New site

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/chasingplastic/chasingplastic.github.io/precache-manifest.bfff949edd3f6aba44fb4bd604a2f404.js"
+  "/precache-manifest.24960695484e562bb6093297c533ad05.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/chasingplastic/chasingplastic.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
